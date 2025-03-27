ba76fd84ba0998dc4d55721b8f8919fb683d5d92
KosmoK <hogankwak@gmail.com>
2025-03-27 19:05:11 -0400

    Updated sketch.JS

--- sketch.js
+++ sketch.js
@@ -1,30 +1,40 @@
-let ball;
 let gamestate = 'mainMenu';
+let startTime = 0;
+let interval = 1000;
+let secondsSinceStart;
 
 function preload() {
-	beeSong = "assets/audio/bee.mp3"
+	beeSong = loadSound("assets/audio/bee.mp3")
+
+	// LOADSONGS
+	bee = new song("assets/audio/bee.mp3",120,65);
+	duckSong = new song("assets/audio/The Duck Song.mp3",186,120)
 }
 
 function setup() {
 	new Canvas(500, 500);
 	displayMode(CENTERED);
 	rectMode(CENTER)
-
-	ball = new Sprite();
-	ball.diameter = 50;
+	colorMode(HSB);
 }
 
 function draw() {
-	background('skyblue');
-	test = new note(0,1);
-
-	text(test.key,width/2 + 100,height/2);
+	secondsSinceStart = millis() / 1000;
+	if(gamestate == 'mainMenu'){
+		mainMenu();
+	}
+	else if(gamestate == 'settings'){
+		settings();
+	}
+	else if (gamestate == 'characterSelect'){
+		characterSelect();
+	}
 
 	if (mouse.presses()) {
-		ball.speed = 10;
-		ball.moveTo(mouse);
+		bee.playSong();
+
+		text(bee.bpm,width/2 + 100,height/2);
 	}
-	mainMenu()
 }
 
 // GAMESTATES
@@ -38,75 +48,91 @@ function characterSelect(){
 }
 function mainMenu(){
 	background('skyblue');
-	fill("blue");
-	rect(252, 140, 190, 70, 20);
-	text("Start", 250, 50);
 	
 	textAlign(CENTER);
-}
-function songSelectionScreen(){
-	background('skyblue');
-}
-function songPlay(){
-	background('skyblue');
+	textSize(60);
+	text("RHYTHM GAME", width/2, 120);
+	
+	stroke("white");
+	rect(247, 180, 90, 30, 10); // button 1
+	rect(245, 215, 90, 30, 10); // button 2
+	rect(245, 250, 90, 30, 10); // button 3
+	
+	textAlign(CENTER);
+	textSize(15);
+	text("PLAY", 290, 200);
+	text("SETTINGS", 290, 234);
+	text("EXIT", 290, 269);
+  
 }
 
 // CLASSES - HOGAN ONLY ZONE (Unless you know what is happening)
 class note{
-	key;
-	time;
-	progress = 0;
-	lyric;
+	#key;
+	#time;
+	#duration = 0;
+	#lyric;
 
-	constructor(key, time, lyric = null){
-		this.key = key;
-		this.time = time;
-		this.lyric = lyric;
+	constructor(key, time, duration = 1, lyric = null){
+		this.#key = key;
+		this.#time = time;
+		this.#lyric = lyric;
 	}
 
-	get key(){return this.key;};
-	get time(){return this.time;};
-	get lyric(){return this.lyric;};
+	// Getters
+	getKey(){return this.#key;};
+	getTime(){return this.#time;};
+	getLyric(){return this.#lyric;};
+	getDuration(){return this.#duration;}; 
 }
 class song{
-	static allAudio;
+	#audio;
+	#length; // ITS IN SECONDS
+	#notes;
+	#speedMulti;
+	#bpm;
+	#bps;
 
-	audio;
-	length;
-	notes;
-	speedMulti;
+	#playTime; // Timer goes from 0ms to end-of-song-ms
 
-	playTime; // Timer goes from 0ms to end-of-song-ms
+	constructor(audio, bpm, length, notes, speedMulti = 1){
+		this.#audio = loadSound(audio);
+		this.#length = length;
+		this.#notes = notes;
+		this.#bpm = bpm;
+		this.#playTime = 0;
+		this.#speedMulti = speedMulti;
 
-	constructor(audio, bpm, length, notes = []){
-		if(allAudio == null){
-			allAudio = [audio];
-		}; // Not even sure if this works
-		this.audio = loadSound(audio);
-		append(allAudio, audio);
-
-		this.length = length;
-		this.notes = notes;
-		this.bpm = bpm;
-		this.playTime = 0;
-		this.speedMulti = 1;
+		this.#bps = bpm * 60;
 	}
-
+	/** METHOD FOR DISPLAYING THE NOTES
+	 *  Uses p5 play's tiles syste
+	 *  https://p5play.org/learn/tiles.html
+	 */
+	displayNotes() {
+		
+	}
 	/** METHOD FOR PLAYING THE SONG
 	 * 	Plays audio, scrolls note, etc...
 	 * 	If speed multi is not specified, plays at last speed multi
 	 */
-	play(speedMulti = this.speedMulti) {
-		this.speedMulti = speedMulti;		
+	playSong(speedMulti = this.#speedMulti) {
+		this.#speedMulti = speedMulti;
+		this.#audio.play()
+		
+		for(this.#playTime; this.#playTime < Math.ceil(this.#length); this.#playTime++){
+			print("Testing!")
+		}
 	}
 	/** METHOD FOR STOPPING THE SONG
 	 * 	Stops the audio, scroll, etc...
 	 */
-	stop() {
+	stopSong() {
 		TODO // UNIMPLEMENTED
 	}
 
-	get playTime(){return this.playTime;};
-	get audio(){return this.audio;};
-	get notes(){return this.notes;};
+	getPlayTime(){return this.#playTime;};
+	getAudio(){return this.#audio;};
+	getNotes(){return this.#notes;};
+	getBPM(){return this.#bpm}
 }
